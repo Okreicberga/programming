669037249d751dbf632d60ca2c0ddbe9f35318fd
Olga Kreicberga <olgakreicberga@MacBook-Pro-Olga.local>
2021-01-31 20:49:28 +0000

    task week02 mbi

---
+++ helloword.py
@@ -0,0 +1,5 @@
+# helloworld.py      
+# This program just prints out hello World 
+# Autor: Olga Kreicberga 
+
+print ("HEllo Word! Olga!")
---
+++ week02/mbi.py
@@ -0,0 +1,6 @@
+# Calculate body max index by a weihgt devide height
+# Author: Olga Kreicberga 
+
+height = float(input("Input your height in meters: "))
+weight = float(input("Input your weight in kilogram: "))
+print("Your body mass index is: ", (weight / (height**2)))

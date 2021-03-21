f5caaeb6884d505a13890d44329bf1854706de70
Olga Kreicberga <g00398805@gmit.ie>
2021-03-21 20:52:14 +0000

    Week 09 Upload

---
+++ labs/Topic09-errors/myFunctions.py
@@ -0,0 +1,49 @@
+# Function Fibonacci that makes a numbers and returns a list containing a Fibonacci sequence of that many numbers. 
+# Author: Olga Kreicberga 
+import logging
+#logging.basicConfig(level=logging.DEBUG)
+
+
+def fibonacci (number):
+    if number < 0:
+        raise ValueError('number must be > 0')
+    if number == 0:
+        return []
+    a = 0
+    b = 1
+    fibonacciSequence = [0]
+    # we have one in the list already so number - 1 times
+    # this isnot the most efficient code
+    # could have used yield
+    for i in range(1,number):
+        fibonacciSequence.append(b)
+        # this is funky code make a = b and b = a + b
+        a , b = b, a + b
+    logging.debug("%d: %s",number, fibonacciSequence)
+    return fibonacciSequence
+
+
+if __name__ == '__main__' :
+# test will go here
+    return7 = [0,1,1,2,3,4,5,8]
+    return11 = [0,1,1,2,3,5,8,13,21,34,55]
+    logging.debug("%s", fibonacci(7))
+    assert fibonacci(7) == return7, 'incorrect return value for 7'
+    assert fibonacci(11) == return11, 'incorrect return value for 11'
+    assert fibonacci(0) == [], 'incorrect return value for 0'
+    assert fibonacci(1) == return11, 'incorrect return value for 1'
+    try:
+        fibonacci(-1)
+    except ValueError:
+        # we want this exception to be thrown
+        # so this is an example where we want to do nothing
+        pass
+    else:
+        # if the exception was not thrown we should throw
+        # Assertion error
+        assert False, 'fibonacci missing ValueError'
+        # or
+        # raise AssertionError("fibonacci no valueError")
+
+
+    print("all good")
